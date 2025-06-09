f8e3990da9c53c418bcffab42cb05227c6615c6c
maxi <maxi.negrete@duocuc.cl>
2025-06-09 15:27:27 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'vida-y-salud',
+  webDir: 'www'
+};
+
+export default config;
